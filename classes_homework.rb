63053bc0101cdb99e4765f8f8ce64eab9b36a4a5
derkrisz <krisztian.dergmail.com>
2017-11-06 19:02:06 +0000

    passes test_team_name

--- classes_homework.rb
+++ classes_homework.rb
@@ -1,34 +1,47 @@
-class Student
-attr_reader :name, :cohort
+class Team
 
-  def initialize(name, cohort)
-    @name = name
-    @cohort = cohort
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
   end
 
-
-  def student_name
-    return @name
+  def team_name
+    return @team_name
   end
 
-  def cohort_number
-    return @cohort
-  end
-
-  def set_name(name)
-    @name = name
-  end
-
-  def set_cohort(cohort)
-    @cohort = cohort
-  end
-
-  def talk
-    return "I can talk"
-  end
-
-  def language(language)
-    return "I love #{language}"
-  end
 
 end
+# class Student
+# attr_reader :name, :cohort
+#
+#   def initialize(name, cohort)
+#     @name = name
+#     @cohort = cohort
+#   end
+#
+#
+#   def student_name
+#     return @name
+#   end
+#
+#   def cohort_number
+#     return @cohort
+#   end
+#
+#   def set_name(name)
+#     @name = name
+#   end
+#
+#   def set_cohort(cohort)
+#     @cohort = cohort
+#   end
+#
+#   def talk
+#     return "I can talk"
+#   end
+#
+#   def language(language)
+#     return "I love #{language}"
+#   end
+# end
--- specs/classes_homework_spec.rb
+++ specs/classes_homework_spec.rb
@@ -2,40 +2,45 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../classes_homework.rb')
 
-class TestStudent < MiniTest::Test
+class TestTeam < MiniTest::Test
 
-def test_return_name
-  student = Student.new('Krisz', 17)
-  assert_equal('Krisz', student.student_name)
+  def test_return_team_name
+    team = Team.new('lovers', ['john','andrew','gary'], 'bill')
+    assert_equal('lovers', team.team_name)
+  end
 end
 
-def test_return_cohort
-  student = Student.new('Krisz', 17)
-  assert_equal(17, student.cohort_number)
-end
-
-def test_set_student_name
-  student = Student.new('Krisz', 17)
-  student.set_name('Adam')
-  assert_equal('Adam', student.name)
-end
-
-def test_set_cohort
-  student = Student.new('Krisz', 17)
-  student.set_cohort(18)
-  assert_equal(18, student.cohort)
-end
-
-def test_student_talks
-  student = Student.new('Krisz', 17)
-  assert_equal("I can talk", student.talk)
-end
-
-def test_favourite_language
-  student = Student.new('Krisz', 17)
-  assert_equal("I love ruby", student.language("ruby"))
-end
-
-
-
-end
+# class TestStudent < MiniTest::Test
+#
+#   def test_return_name
+#     student = Student.new('Krisz', 17)
+#     assert_equal('Krisz', student.student_name)
+#   end
+#
+#   def test_return_cohort
+#     student = Student.new('Krisz', 17)
+#     assert_equal(17, student.cohort_number)
+#   end
+#
+#   def test_set_student_name
+#     student = Student.new('Krisz', 17)
+#     student.set_name('Adam')
+#     assert_equal('Adam', student.name)
+#   end
+#
+#   def test_set_cohort
+#     student = Student.new('Krisz', 17)
+#     student.set_cohort(18)
+#     assert_equal(18, student.cohort)
+#   end
+#
+#   def test_student_talks
+#     student = Student.new('Krisz', 17)
+#     assert_equal("I can talk", student.talk)
+#   end
+#
+#   def test_favourite_language
+#     student = Student.new('Krisz', 17)
+#     assert_equal("I love ruby", student.language("ruby"))
+#   end
+# end
